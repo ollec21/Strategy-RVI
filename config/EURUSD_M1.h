571605ad6c3363c9ea38973ee62c2125a080030d
kenorb <kenorb@users.noreply.github.com>
2021-01-17 22:38:20 +0000

    Optimize Indicator params (pf=0.37; dd=16.15; tt=17350)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_RVI_Params_M1 : RVIParams {
   Indi_RVI_Params_M1() : RVIParams(indi_rvi_defaults, PERIOD_M1) {
-    period = 20;
+    period = 12;
     shift = 0;
   }
 } indi_rvi_m1;
@@ -18,12 +18,12 @@ struct Stg_RVI_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
